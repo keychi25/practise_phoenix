6143dc36db4e35fb6e53dd884c2cc1a9adb774cf
keychi25 <e165412y@st.u-gakugei.ac.jp>
2019-02-25 17:35:27 +0900

    gitignoreの追加 / Gravatorの削除

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,8 +1,3 @@
 defmodule PractisePhoenix.UserView do
   use PractisePhoenix.Web, :view
-  alias PractisePhoenix.{User, Gravatar}
-
-  def get_gravatar_url(%User{email: email}) do
-    Gravatar.get_gravatar_url(email)
-  end
 end
